e92822e1324e550ec24f934f67450478525ade46
BemBalls <11820033@usc.edu.ph>
2025-07-12 14:33:34 +0800

    1 stuff

--- db/user.sql
+++ db/user.sql
@@ -4,6 +4,7 @@ CREATE TABLE `Users` (
   `name_id` bigint(11) NOT NULL,
   FOREIGN KEY (`name_id`) REFERENCES Names(`name_id`),
   `password` varchar(255) NOT NULL,
-  `date_created` date NOT NULL DEFAULT CURRENT_DATE
-  
+  `date_created` date NOT NULL DEFAULT CURRENT_DATE,
+  `staff_type` enum('customer', 'inventory', 'staff') NOT NULL
 )
+
