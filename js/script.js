4b7fdcc926eced2cbadb8e8d3e674687262048d7
crowsified <24102571@usc.edu.ph>
2025-07-15 12:24:58 +0800

    category done

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@ function filterProducts(type) {
   document.getElementById('newButton').classList.remove('active');
   document.getElementById('filteredButton').classList.remove('active');
 
-  document.getElementById('allButton').classList.add('inactive');
-  document.getElementById('newButton').classList.add('inactive');
-  document.getElementById('filteredButton').classList.add('inactive');
+  // document.getElementById('allButton').classList.add('inactive');
+  // document.getElementById('newButton').classList.add('inactive');
+  // document.getElementById('filteredButton').classList.add('inactive');
 
   if (type === 'all') {
     document.getElementById('allButton').classList.add('active');
